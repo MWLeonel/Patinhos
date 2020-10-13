a7a985c6cd0b7067d08032f7dfed25cdf97e29d4
MWLeonel <wmleonel6@gmail.com>
2020-10-13 18:39:49 -0300

    Musica dos Patinhos

---
+++ Patinhos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Patinhos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pato;
+
+            Console.Write("Digite o número de patinhos? ");
+            pato = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = pato; i > 0; i -= 1)
+            {
+                Console.WriteLine(i + " Patinhos foram passear");
+                Console.WriteLine("Além das montanhas");
+                Console.WriteLine("Para brincar");
+                Console.WriteLine("A mamãe gritou: Quá, quá, quá, quá");
+                Console.WriteLine("Mas só " + (i - 1) + " patinhos voltaram de lá.\n");
+
+            }
+
+            Console.WriteLine("A mamãe patinha foi procurar");
+            Console.WriteLine("Além das montanhas");
+            Console.WriteLine("Na beira do mar");
+            Console.WriteLine("A mamãe gritou: Quá, quá, quá, quá");
+            Console.WriteLine("E os " + pato + " patinhos voltaram de lá.");
+        }
+    }
+}
